74b0e922c01b56af8d9f37a3eed7608674a4cd62
Matt Kim <mingyuya@gmail.com>
2017-12-26 23:14:17 +0900

    done calculating errors

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,28 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_i, double Ki_i, double Kd_i) {
+  Kp = Kp_i;
+  Ki = Ki_i;
+  Kd = Kd_i;
+
+  p_error = 0.;
+  i_error = 0.;
+  d_error = 0.;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error; 
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
 }
 
+vector<int> PID::Twiddle(double tolerance) {
+  vector<double> p;
+  p.resize(3);
+
+  return p; 
+}
